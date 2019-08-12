b5a1837430358019495dab5a7c6a5cd95013ed94
Norin Stark <33775952+NorinStark@users.noreply.github.com>
2019-08-12 15:11:54 +0700

    Update app.py
    route to html

--- app.py
+++ app.py
@@ -1,49 +1,47 @@
 import os
-import json
-from bson.json_util import dumps
-
-
-import requests
-from flask import Flask, request, url_for
 from pymongo import MongoClient
-
+from flask import Flask, request, render_template
+from bson.json_util import dumps
 
 app = Flask(__name__)
 
-#Setting Environment for MongoDB
 MONGO_URI = 'mongodb://dev:sousdey123@ds219676-a0.mlab.com:19676,ds219676-a1.mlab.com:19676/heroku_pkl108wc?replicaSet=rs-ds219676'
 mongo = MongoClient(MONGO_URI)
-db = mongo.heroku_pkl108wc  #connect with the database's name
+mydb = mongo.heroku_pkl108wc
+mycol = mydb["customer_phones"]
+
+# MONGO_URI = 'mongodb://sixsixone5:42577400Vj@ds133137.mlab.com:33137/heroku_mck6pbmw'
+# mongo = MongoClient(MONGO_URI)
+# mydb = mongo.heroku_mck6pbmw
+# orderCollection = mydb.web_orders
+
+# print("hello this is a value of customer_phones",mycol)
 
 # @app.route('/')
 # def hello():
 #     return "Hello World!"
-#
-#
-# @app.route('/<name>')
-# def hello_name(name):
-#     return "Hello {}!".format(name)
 
 @app.route("/api/phones", methods = ['GET'])
 def show():
     page_id = request.args.get('pageid')
     print("==> ", page_id)
     try:
-        # phones = db.customer_phones.find( WHERE condition, selected_field ).limit(10)
-        selected_fields = {
-            'PAGE_ID': 1,
-            'PAGE_NAME': 1,
-            '_id': 0
-        }
-        condition = {
-            "PAGE_ID": page_id,
-        } if page_id else {}
+        # phones = mydb.customer_phones.find( WHERE condition, selected_field ).limit(10)
+        # selected_fields = {
+        #     'PAGE_ID': 1,
+        #     'PAGE_NAME': 1,
+        #     '_id': 0
+        # }
+        # condition = {
+        #     "PAGE_ID": page_id,
+        # } if page_id else {}
 
         # var_name = value_if_CONDITION_is_true if CONDITION else value_if_CONDITION_is_false
 
-        phones = db.customer_phones.find(condition, selected_fields).limit(100)
-        # for phone in phones:
-        #     print("-=-> ", phone)
+        # orders = myCollection.find()
+        phones = mycol.find().limit(50)
+        # for order in orders:
+        #     print("-=-> ", order)
 
         data = {
             'phones': phones,
@@ -53,6 +51,15 @@ def show():
         return dumps("There is a problem")
     return dumps(data)
 
+
+###### Page ######
+@app.route('/')
+def index():
+    # get data
+    customers_data = list(mycol.find().limit(100))
+    # for phone in phone_numbers:
+    #     print(phone)
+    return render_template('index.html', data=customers_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
